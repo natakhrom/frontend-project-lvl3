eabb6e5f17547eaaa4202efb9945588932a36bfd
Natalia <natakhrom2701@yandex.ru>
2022-06-30 14:43:49 +0200

    make buld

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist',
   },
   mode: 'development',
   devServer: {
