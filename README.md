1c337f0f1c502f98ab3dc5614ace112c18039295
Natalia <natakhrom2701@yandex.ru>
2022-06-30 13:59:13 +0200

    install eslint-plugin-jest

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 [![Actions Status](https://github.com/natakhrom/frontend-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/natakhrom/frontend-project-lvl3/actions)
 
 ### Tests status
+[![Linter status](https://github.com/natakhrom/frontend-project-lvl3/actions/workflows/thirdProject-check/badge.svg)](https://github.com/natakhrom/frontend-project-lvl3/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/653b4d0255d3d2f69412/maintainability)](https://codeclimate.com/github/natakhrom/frontend-project-lvl3/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/653b4d0255d3d2f69412/test_coverage)](https://codeclimate.com/github/natakhrom/frontend-project-lvl3/test_coverage)
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,58 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-        publicPath: ''
+    publicPath: '',
   },
   mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'),
     compress: true,
     port: 8080,
-    open: true 
+    open: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: 'babel-loader',
-        exclude: '/node_modules/'
+        exclude: '/node_modules/',
       },
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader'
-        }, 'postcss-loader']
+          loader: 'css-loader',
+        }, 'postcss-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          'style-loader',
           // Translates CSS into CommonJS
-          "css-loader",
+          'ss-loader',
           // Compiles Sass to CSS
-          "sass-loader",
+          'sass-loader',
         ],
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Custom template',
-      template: 'index.html'
+      template: 'index.html',
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
-  ]
-}
+    new MiniCssExtractPlugin(),
+  ],
+};
